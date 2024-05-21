2d9f5e31e30f989ab804a6c001dc536e160a3cdc
4ugeistr <andrii.dutko@gmail.com>
2024-05-21 10:48:20 +0300

    26.05 fix., hours

--- flow/20230905_templates/InsertIntoLiturgy.py
+++ flow/20230905_templates/InsertIntoLiturgy.py
@@ -8,9 +8,11 @@ RGB_RED = RGBColor(0xff, 0x44, 0x00)
 #mode='g'
 #cur_month='Липень'
 month_no = datetime.now().month+1 if datetime.now().month!=12 else 1
-#month_no=5
-#print("WARNING. Month_no OVERRIDE", month_no)
 year_no = datetime.now().year if datetime.now().month!=12 else datetime.now().year+1
+
+month_no=5
+print("WARNING. Month_no OVERRIDE", month_no)
+
 mode = easygui.choicebox('u - Юліанський, g - Григоріанський', 'Вибір календаря', ['u','g'])
 
 #dirs = easygui.diropenbox()
