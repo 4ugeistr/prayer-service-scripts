fe5a38ecacc32d23b63b0f1d3c831b2810bc1df1
4ugeistr <andrii.dutko@gmail.com>
2024-11-25 10:35:26 +0200

    updated json

--- flow/зачала - перевірка/convert_csv_to_json.py
+++ flow/зачала - перевірка/convert_csv_to_json.py
@@ -3,29 +3,31 @@ import json
 
 dataset={1:'apostol',
          2:'evanhelie'}
-chosen = int(input("1 - apostol, 2 - evanhelie\n") )
+#chosen = int(input("1 - apostol, 2 - evanhelie\n") )
 
 
-csv_file = open(f'{dataset[chosen]}.csv', 'r', encoding='utf-8')
-json_file = open(f'{dataset[chosen]}.json', 'w', encoding='utf-8')
+for chosen in list(range(1,3)):
+    csv_file = open(f'{dataset[chosen]}.csv', 'r', encoding='utf-8')
+    json_file = open(f'{dataset[chosen]}.json', 'w', encoding='utf-8')
 
-fieldnames = ('reading', 'book', 'zach_num', 'zach_abc')
-reader = csv.DictReader(csv_file, fieldnames)
+    fieldnames = ('reading', 'book','book_code', 'zach_num', 'zach_abc')
+    reader = csv.DictReader(csv_file, fieldnames)
 
-# Skip the header row
-next(reader)
+    # Skip the header row
+    next(reader)
 
-data = []
-for row in reader:
-    # Skip empty rows
-    if not any(row.values()):
-        continue
-    del row[None]
-    #print(row)2
-    #print({})
-    data.append(row)
+    data = []
+    for row in reader:
+        # Skip empty rows
+        if not any(row.values()):
+            continue
+        del row[None]
+        #print(row)2
+        #print({})
+        data.append(row)
 
-json.dump(data, json_file, ensure_ascii=False)
+    json.dump(data, json_file, ensure_ascii=False)
 
-csv_file.close()
-json_file.close()
+    csv_file.close()
+    json_file.close()
+    print(f"Finished converting {dataset[chosen]}.json.")
