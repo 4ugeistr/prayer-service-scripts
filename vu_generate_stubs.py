24782b8f4f47043554e774c29c9fe7855a66dec7
Andrii Dtk <andrii.dutko@gmail.com>
2026-04-05 09:28:09 +0300

    may WIP

--- vu_generate_stubs.py
+++ vu_generate_stubs.py
@@ -84,91 +84,6 @@ if mode == 'u':
 elif mode == 'g':
     mode_suffix='Гр'
     mode_suffix2='ГР'
-'''
-def copy_paragraph_before(p_to_insert_before,source_paragraph):
-    target_paragraph = p_to_insert_before.insert_paragraph_before()
-    target_paragraph.style = source_paragraph.style
-    target_paragraph.alignment = source_paragraph.alignment
-    for run in source_paragraph.runs:
-        new_run = target_paragraph.add_run(run.text)
-        new_run.bold = run.bold
-        new_run.italic = run.italic
-        new_run.underline = run.underline
-        new_run.font.size = run.font.size
-        new_run.font.name = run.font.name
-        new_run.font.color.rgb = run.font.color.rgb
-        new_run.font.highlight_color = run.font.highlight_color
-'''
-'''
-def delete_paragraph(paragraph):
-    p = paragraph._element
-    p.getparent().remove(p)
-    p._p = p._element = None
-
-def delete_run(run):
-    p = run._element
-    p.getparent().remove(p)
-    p._p = p._element = None
-
-def copy_run(target_paragraph,run):
-    new_run = target_paragraph.add_run(run.text)
-    #KeyError: "no style with name 'Default Paragraph Font'"
-        
-    new_run.bold = run.bold
-    new_run.italic = run.italic
-    new_run.underline = run.underline
-    new_run.font.superscript = run.font.superscript
-    #new_run.font.size = run.font.size
-    #new_run.font.name = run.font.name
-    new_run.font.name='Times New Roman'
-    new_run.font.size=152400
-    new_run.font.color.rgb = run.font.color.rgb
-    new_run.font.highlight_color = run.font.highlight_color
-
-def copy_paragraph_before(paragraph_to_insert_before,source_paragraph):
-    target_paragraph = paragraph_to_insert_before.insert_paragraph_before()
-    try:
-        target_paragraph.style = source_paragraph.style
-        target_paragraph.paragraph_format.space_after = source_paragraph.paragraph_format.space_after
-        #target_paragraph.style.font = source_paragraph.style.font.name
-    except KeyError:
-        print(f"Warning. Text {source_paragraph.text[:20]} has style{source_paragraph.style}")
-        pass
-    for run in source_paragraph.runs:
-        copy_run(target_paragraph,run)
-    return target_paragraph
-
-def copy_paragraph_list_before(p_to_insert_before,paragraph_list):
-    #print("qty of p to insert:",len(paragraph_list))
-    for p in paragraph_list:
-        copy_paragraph_before(p_to_insert_before,p)
-
-
-
-
-def copy_paragraph(target_doc,source_paragraph):
-    target_paragraph = target_doc.add_paragraph()
-    try:
-        target_paragraph.style = source_paragraph.style
-        target_paragraph.paragraph_format.space_after = Pt(6)
-    except KeyError:
-        print(f"Warning. Text {source_paragraph.text[:20]} has style{source_paragraph.style}")
-        pass
-    target_paragraph.alignment = source_paragraph.alignment
-    for run in source_paragraph.runs:
-        new_run = target_paragraph.add_run(run.text)
-        new_run.bold = run.bold
-        new_run.italic = run.italic
-        new_run.underline = run.underline
-        new_run.font.size = run.font.size
-        new_run.font.name = run.font.name
-        new_run.font.color.rgb = run.font.color.rgb
-        new_run.font.highlight_color = run.font.highlight_color
-
-def copy_paragraph_list(target_doc,paragraph_list):
-    for p in paragraph_list:
-        copy_paragraph(target_doc,p)
-'''
 
 def get_octoechos_template_files(pattern):
     template_dic = {}
@@ -1473,6 +1388,9 @@ else:
 if glob.glob(f'docx_resources\\Вечірня-Утреня\\tmp_тропарі_спеціальні\\тропарі-спеціальні-{month_no:02}.docx'):
     troparia_special = get_menaion_troparia_texts(
         glob.glob(f'docx_resources\\Вечірня-Утреня\\tmp_тропарі_спеціальні\\тропарі-спеціальні-{month_no:02}.docx')[0])
+elif glob.glob(f'docx_resources\\Вечірня-Утреня\\tmp_тропарі_святкові\\тропарі-спеціальні-{month_no:02}-{mode_suffix2}.docx'):
+    troparia_special = get_menaion_troparia_texts(
+        glob.glob(f'docx_resources\\Вечірня-Утреня\\tmp_тропарі_святкові\\тропарі-спеціальні-{month_no:02}-{mode_suffix2}.docx')[0])
 else:
     troparia_special = None
 
